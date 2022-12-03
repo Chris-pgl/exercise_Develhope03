6709fab39902d42e5d5766fdfe9a41c30a25a6b3
Chris <cr1s@hotmail.it>
2022-12-03 16:47:51 +0100

    Add: Exercise03

---
+++ src/House.java
@@ -0,0 +1,11 @@
+public class House {
+
+    public String addres;
+
+    public int numberOfFloor;
+
+
+    public House() {
+        System.out.println("The house is created");
+    }
+}
---
+++ src/Tester.java
@@ -0,0 +1,16 @@
+public class Tester {
+
+    public static void main(String[] args) {
+
+        House house1 = new House();
+        house1.addres = "groowStreet 178";
+        house1.numberOfFloor = 2;
+
+
+        House house2 = new House();
+        house2.addres = "groowStreet 174";
+        house2.numberOfFloor = 1;
+
+
+    }
+}
